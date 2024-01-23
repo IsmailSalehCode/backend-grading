1d71db57be409ea909e5e6d77f068f84ee4faf6d
Ismail Saleh <centriu78@gmail.com>
2024-01-24 00:05:11 +0200

    +

--- src/main/java/com/ismail/gradesubmission/security/SecurityConstants.java
+++ src/main/java/com/ismail/gradesubmission/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.ismail.gradesubmission.security;
 
 public class SecurityConstants {
-    public static final String SECRET_KEY = "f_QiDhWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&W";
+    public static final String SECRET_KEY = "f_QiDhWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/6?D*G-KaPdSgVkYp3s6v9y$B&W";
     public static final int TOKEN_EXPIRATION = 7200000; // 7200000 milliseconds = 7200 seconds = 2 hours.
     public static final String BEARER = "Bearer "; // Authorization : "Bearer " + Token
     public static final String AUTHORIZATION = "Authorization"; // "Authorization" : Bearer Token
